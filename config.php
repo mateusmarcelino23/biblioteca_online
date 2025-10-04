a3b3da200bc8647da75b47cf4d0d4649087b32cc
alef-ss <alef-ss@gmail.com>
2025-10-03 21:13:37 -0300

    arquivo de conexão atualizado, tá usando as variáveis do .env

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$env = parse_ini_file(__DIR__ . '/.env');
+
+$host = $env['DB_HOST'];
+$db   = $env['DB_NAME'];
+$user = $env['DB_USER'];
+$pass = $env['DB_PASS'];
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+  die("Falha na conexão: " . $conn->connect_error);
+}
+echo "Conexão OK!";
